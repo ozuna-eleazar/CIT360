3ba5b00a354feaf82c6e6cecc86f2aaade53210c
ozuna-eleazar <eleazar.ozuna@gmail.com>
2015-06-30 11:58:41 -0600

    Url update

---
+++ Sandbox/src/javaHttpUrlConnect/JavaUrlConnectionReader.java
@@ -0,0 +1,48 @@
+package javaHttpUrlConnect;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class JavaUrlConnectionReader {
+
+	public static void main(String[] args)
+	  {
+	    String output  = getUrlContents("http://lynnpagelphotography.com/");
+	    System.out.println(output);
+	  }
+	 
+	  private static String getUrlContents(String theUrl)
+	  {
+	    StringBuilder content = new StringBuilder();
+	 
+	    try
+	    {
+	      // create a url object
+	      URL url = new URL(theUrl);
+	 
+	      // create a urlconnection object
+	      URLConnection urlConnection = url.openConnection();
+	 
+	      // wrap the urlconnection in a bufferedreader
+	      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	 
+	      String line;
+	 
+	      // read from the urlconnection via the bufferedreader
+	      while ((line = bufferedReader.readLine()) != null)
+	      {
+	        content.append(line + "\n");
+	      }
+	      bufferedReader.close();
+	    }
+	    catch(Exception e)
+	    {
+	      e.printStackTrace();
+	    }
+	    return content.toString();
+	  }
+	
+	
+}
