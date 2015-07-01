05f4418383deec86a9983e7dc1705059f17cfb23
ozuna-eleazar <eleazar.ozuna@gmail.com>
2015-06-30 21:06:26 -0600

    Update Http

--- Sandbox/src/javaHttpUrlConnect/JavaHttpUrlConnect.java
+++ Sandbox/src/javaHttpUrlConnect/JavaHttpUrlConnect.java
@@ -8,76 +8,60 @@ import java.net.URL;
 
 public class JavaHttpUrlConnect {
 
-	 public static void main(String[] args)
-			  throws Exception
-			  {
-			    new JavaHttpUrlConnect();
-			  }
-			   
-			  public JavaHttpUrlConnect()
-			  {
-			    try
-			    {
-			      String myUrl = "http://lynnpagelphotography.com/";
-			      
-			      String results = doHttpUrlConnectionAction(myUrl);
-			      System.out.println(results);
-			    }
-			    catch (Exception e)
-			    {
-			     
-			    }
-			  }
-			 
-			  private String doHttpUrlConnectionAction(String myUrl)
-			  throws Exception
-			  {
-			    URL url = null;
-			    BufferedReader reader = null;
-			    StringBuilder stringBuilder;
-			 
-			    try
-			    {
-			      // create the HttpURLConnection
-			      url = new URL(myUrl);
-			      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			       
-			      // give it 15 seconds to respond
-			      connection.setReadTimeout(15*1000);
-			      connection.connect();
-			 
-			      // read the output from the server
-			      reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			      stringBuilder = new StringBuilder();
-			 
-			      String line = null;
-			      while ((line = reader.readLine()) != null)
-			      {
-			        stringBuilder.append(line + "\n");
-			      }
-			      return stringBuilder.toString();
-			    }
-			    catch (Exception e)
-			    {
-			      e.printStackTrace();
-			      throw e;
-			    }
-			    finally
-			    {
-			      // close the reader; this can throw an exception too, so
-			      // wrap it in another try/catch block.
-			      if (reader != null)
-			      {
-			        try
-			        {
-			          reader.close();
-			        }
-			        catch (IOException ioe)
-			        {
-			          ioe.printStackTrace();
-			        }
-			      }
-			    }
-			  }
-	
+	public static void main(String[] args) throws Exception {
+		new JavaHttpUrlConnect();
+	}
+
+	public JavaHttpUrlConnect() {
+		try {
+			String myUrl = "http://lynnpagelphotography.com/";
+
+			String testing = UrlConnectionAction(myUrl);
+			System.out.println(testing);
+		} catch (Exception e) {
+
+		}
+	}
+
+	private String UrlConnectionAction(String myUrl) throws Exception {
+		URL url = null;
+		BufferedReader reader = null;
+		StringBuilder stringBuilder;
+
+		try {
+			// create the HttpURLConnection
+			url = new URL(myUrl);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+
+			// 15 seconds to respond
+			connection.setReadTimeout(15 * 1000);
+			connection.connect();
+
+			// read the output from the server
+			reader = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			stringBuilder = new StringBuilder();
+
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				stringBuilder.append(line + "\n");
+			}
+			return stringBuilder.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			// close the reader; this can throw an exception too, so
+			// wrap it in another try/catch block.
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
