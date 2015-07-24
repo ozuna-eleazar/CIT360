0df905233c7c41bb032380e88e5a73e926e9d37e
ozuna-eleazar <eleazar.ozuna@gmail.com>
2015-07-24 12:23:04 -0500

    Updated Json

--- Sandbox/src/json/Json.java
+++ Sandbox/src/json/Json.java
@@ -12,9 +12,29 @@ public static void main(String[] args) {
 	baseball.put("Player","Craig Biggio");
 	baseball.put("Jersy Number","7");
 	baseball.put("Position","Second Base");
-	baseball.put("Team","Houston Astros");
-	
+	baseball.put("Team","Astros");
+	baseball.put("City", "Houston");
+
 	System.out.println(baseball);
+	System.out.println(baseball.size());
+	//Looking for the key "City" 
+	System.out.println(baseball.containsKey("City"));
+	//Since the Key is the left one it does not find Houston because Houston is a value.
+	System.out.println(baseball.containsKey("Houston"));
+	System.out.println();
+	
+	JSONObject football = new JSONObject();
+	football.put("Team", "Texans");
+	football.put("City","Houston");
+	football.put("Division", "AFC South");
+	
+	System.out.println(football);
+	
+	System.out.println(football.size());
+			
+	
+	
+	
 }
 
 }
